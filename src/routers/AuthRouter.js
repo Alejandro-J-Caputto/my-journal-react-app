9e9ed35f1d3d99a71b51f14234a28c2120103fd6
Alejandro Caputto frias <alejandro_caputto@MacBook-Air-de-Alejandro.local>
2021-01-20 10:34:04 +0100

    routing

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import { LoginScreen } from '../components/Auth/LoginScreen';
+import { RegisterScreen } from '../components/Auth/RegisterScreen';
 export const AuthRouter = () => {
   return (
-    <div>
-      
-    </div>
+    <Switch>
+      <Route exact path="/auth/login" component={LoginScreen} />
+      <Route exact path="/auth/register" component = {RegisterScreen} />
+      <Redirect to="/auth/login"/>
+    </Switch>
   )
 }
